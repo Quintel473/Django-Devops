01dedeb9b0def295910f6eb67a9140227a2808fa
Quintel473 <qunitelcharles@gmail.com>
2025-05-03 21:16:12 -0400

    yep

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('style.css', views.serve_css),  # accessible as /style.css
+    path('style.css', views.serve_css),  # this serves your CSS manually
 ]
+# This is a simple Django URL configuration that maps the root URL to the `home` view and the `/style.css` URL to the `serve_css` view.
+# The `home` view serves an HTML file, while the `serve_css` view serves a CSS file.
--- myapp/views.py
+++ myapp/views.py
@@ -3,13 +3,13 @@ from django.http import HttpResponse
 
 BASE_DIR = os.path.dirname(__file__)
 
-def home(request):
+def home(_request):
     html_path = os.path.join(BASE_DIR, 'home.html')
     with open(html_path, 'r') as f:
         html = f.read()
     return HttpResponse(html)
 
-def serve_css(request):
+def serve_css(_request):
     css_path = os.path.join(BASE_DIR, 'style.css')
     with open(css_path, 'r') as f:
         css = f.read()
